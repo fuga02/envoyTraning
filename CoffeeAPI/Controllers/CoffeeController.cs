c21b43c7fdc74cfff0ab7c1b87139f7a4a0ce50a
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-09-08 07:48:23 -0700

    controllers were added

---
+++ CoffeeAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CoffeeController : ControllerBase
+{
+    private static readonly string[] Coffee = new[]
+    {
+        "Flat White", "Long Black", "Latte", "Americano","Cappuccino"
+    };
+
+    [HttpGet]
+    public ActionResult Get()
+    {
+        var rng = new Random();
+
+        return Ok(Coffee[rng.Next(Coffee.Length)]);
+    }
+}
---
+++ TeaAPI/Controllers/TeaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class TeaController : ControllerBase
+{
+    private static readonly string[] Teas = new[]
+    {
+        "Green","Peppermit","Early Grey", "English Breakfast", "Camomile"
+    };
+
+    [HttpGet]
+    public ActionResult Get()
+    {
+        var rng = new Random();
+
+        return Ok(Teas[rng.Next(Teas.Length)]);
+    }
+}
